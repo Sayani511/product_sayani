5ade223db4a82cbad7e8d545f2d3de5b2dea5a1e
Sayani511 <sayanidas250703@gmail.com>
2025-04-04 13:34:21 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+import Student from "./components/Student";
+import Icons from "./components/Icon";
+import Map from "./components/Map"
+function App() {
+  return (
+    <div className="App">
+     {/* <Student/>*/ }
+     {/*<Icons/>*/}
+     <Map/>
+     
+    </div>
+  );
+}
+
+
+export default App;
